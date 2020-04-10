24b65d7cfa95f9f1621e74ff7a246b84a6782fa5
Omid Adib <akbaradibfar4@gmail.com>
2020-04-10 15:47:55 +0430

    add logout url

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name="index"),
     path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
     path('register/', register, name="register"),
     path('complete_profile/', complete_profile, name="complete_profile"),
 
