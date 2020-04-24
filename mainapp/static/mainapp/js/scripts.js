ca1312993ce5a3d2e893e456961a5db476ccc908
Omid Adib <akbaradibfar4@gmail.com>
2020-04-24 22:08:10 +0430

    add sweetalert library and customize cancel alert

--- mainapp/static/mainapp/js/scripts.js
+++ mainapp/static/mainapp/js/scripts.js
@@ -2,23 +2,30 @@ function cancelYN(pk) {
     let serId = "serial" + pk.toString();
     let serial = document.getElementById(serId);
     let msg = "آیا از ارسال درخواست لغو برای سرویس " + serial.innerHTML + " اطمینان دارید؟";
-    let conf = window.confirm(msg);
-
-    if (conf === true) {
-        let xhttp;
-        xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "/cancel?pk=" + pk, true);
-        xhttp.send();
-        xhttp.onreadystatechange = function () {
-            var result = JSON.parse(this.response);
-            var status = JSON.parse(result["status"]);
-            if (status == 200) {
-                location.reload();
-            } else if (status == 201) {
-                location.reload();
+    swal({
+        title: "توجه!",
+        text: msg,
+        icon: "warning",
+        buttons: ["بیخیال", "لغو کن"],
+        dangerMode: true,
+    })
+        .then(value => {
+            if (value) {
+                let xhttp;
+                xhttp = new XMLHttpRequest();
+                xhttp.open("GET", "/cancel?pk=" + pk, true);
+                xhttp.send();
+                xhttp.onreadystatechange = function () {
+                    var result = JSON.parse(this.response);
+                    var status = JSON.parse(result["status"]);
+                    if (status == 200) {
+                        location.reload();
+                    } else if (status == 201) {
+                        location.reload();
+                    }
+                }
             }
-        }
-    }
+        });
 }
 
 function profileHover() {
