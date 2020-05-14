10800bb39d716401e962e22a598ad75065a60367
Omid Adib <akbaradibfar4@gmail.com>
2020-05-14 15:57:17 +0430

    add new cron job

--- mainapp/cron.py
+++ mainapp/cron.py
@@ -2,7 +2,7 @@ import datetime
 
 import kronos
 
-from .models import Request
+from .models import Request, ExtendRequest
 
 
 @kronos.register('58 23 * * *')
@@ -15,12 +15,27 @@ def expire_outdated_requests():
 
 
 @kronos.register('55 23 * * *')
-def reject_requests_not_response_by_3_days():
+def reject_requests_not_payed_by_3_days():
     req_list = Request.objects.filter(acceptance_status="Paying")
     for _ in req_list:
         days_between_requested = _.date_requested - datetime.date.today()
         days = abs(days_between_requested.days)
         if days >= 3:
             _.acceptance_status = "Rej"
+            _.date_expired = None
             _.description = "درخواست شما بدلیل عدم پرداخت هزینه سرویس در مدت معین، رد شد"
             _.save()
+
+
+@kronos.register('52 23 * * *')
+def reject_extends_not_payed_by_3_days():
+    ext_list = ExtendRequest.objects.filter(acceptance_status="Paying")
+    for _ in ext_list:
+        days_between_requested = _.date_requested - datetime.date.today()
+        days = abs(days_between_requested.days)
+        if days >= 3:
+            _.acceptance_status = "Rej"
+            _.request.acceptance_status = "Rej"
+            _.request.description += "| تمدید شما بدلیل عدم پرداخت هزینه در مدت معین، رد شد"
+            _.save()
+            _.request.save()
