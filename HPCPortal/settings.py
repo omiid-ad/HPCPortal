331933bd672e0310702584d389d6284c307af61f
Omiid Adibfar <omid.adibfar@yahoo.com>
2021-05-23 21:41:29 +0430

    remove trailing slash from MEDIA_ROOT

--- HPCPortal/settings.py
+++ HPCPortal/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
